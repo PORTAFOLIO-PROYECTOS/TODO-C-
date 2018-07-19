774b7d56d9d2d5dd57ca25157474bccab2e97e03
Hugo <hugo.rock20@hotmail.com>
2018-07-18 23:32:41 -0500

    todo c shrap

---
+++ app/1-Introducción/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Introducción
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            Console.ReadLine();
+        }
+    }
+}
