ac730bc89deb0d06eda57aa9f986c6c3205c2693
HugoRoca <hugo.rock20@hotmail.com>
2018-09-03 11:34:39 -0500

    4- Bucles

---
+++ app/1-Introducción/4-Bucles.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Introducción
+{
+    class _4_Bucles
+    {
+        /*
+         En este capítulo del curso mostraré varios ejemplos sobre algunas estructuras iterativas que nos permiten efectuar 
+         operaciones n veces de forma consecutiva.Los bucles. En el video muestro con ejemplos sencillos el funcionamietos de estos tres bucles:
+
+
+         Bucle while : En primer lugar se evalua la condición, si la misma resulta verdadera se ejecutan las operaciones que se encuentren dentro 
+         del bloque de código que se encuentre entre las dos llaves mientras la condición sea cumpla:
+         
+            While( condicion a evaluar)
+            {
+             //Código a ejecutar
+            }
+
+
+        Bucle do while: La condición se evalua al finalizar la estructura. Esta estructura se utiliza cuando conocemos de antemano que por lo menos 
+        una vez se ejecutará el bloque repetitivo.Se ejecuta hasta que la condicion sea falsa:
+            
+            {
+             //Código a ejecutar
+            }
+            do while( condicion a evaluar) 
+
+
+        Bucle for: Cualquier problema que requiera una estructura repetitiva se puede resolver empleando la estructura while. La estructura for se 
+        usa en aquellas situaciones en las cuales sabemos la cantidad de veces que queremos que se ejecute el bloque de instrucciones.Por ejemplo:
+
+
+            for(int i=0; i<120;i++ )
+            {
+             //Código que se ejecutaria 120 veces
+            }
+         
+         */
+    }
+}
